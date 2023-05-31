caa568a84ed532c3f24f0a2f3de566c67997aed0
Android-JCPP-Servo <astewart1138@gmail.com>
2023-05-31 16:50:59 -0500

    Add proper colors to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,33 +2,44 @@ import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">My Portfolio</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <Link className="nav-link" aria-current="page" to="/">Home</Link>
                     </li>
+
                     <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link" href="https://www.linkedin.com/in/andersen-stewart-57547220b/" target="_blank">LinkedIn</a>
                     </li>
+
+                    <li className="nav-item">
+                        <a className="nav-link" href="https://github.com/Android-JCPP-Servo" target="_blank">GitHub</a>
+                    </li>
+
                     <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><hr className="dropdown-divider"/></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                        <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Projects</Link>
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><hr className="dropdown-divider"/></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
                     </li>
-                    <li className="nav-item">
-                    <a className="nav-link disabled">Disabled</a>
+
+                    <li className="nav-item dropdown">
+                        <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Certifications</Link>
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><hr className="dropdown-divider"/></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
                     </li>
                 </ul>
                 <form className="d-flex" role="search">
