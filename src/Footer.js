846801924c3829c71f7f2ff49c86018e617c8650
Android-JCPP-Servo <astewart1138@gmail.com>
2023-06-02 18:05:36 -0500

    Fix Footer to show how site was built

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="container mb-5">
             <div className="row">
                 <div className="col-sm-12 text-center text-black-50">
-                    &copy; 2023&emsp;|&emsp;Andersen Stewart's Portfolio&emsp;|&emsp;Designed using Bootstrap
+                    &copy; 2023&emsp;|&emsp;Andersen Stewart's Portfolio&emsp;|&emsp;Designed using React.js and Bootstrap
                 </div>
             </div>
         </div>
