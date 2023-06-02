87b45e53615d682af269fd742f6f351805b6ca8d
Android-JCPP-Servo <astewart1138@gmail.com>
2023-06-02 17:56:11 -0500

    Add NarrowImageCard for school, work, and volunteer

---
+++ src/NarrowImageCard.js
@@ -0,0 +1,18 @@
+import {Link} from 'react-router-dom'
+
+const NarrowImageCard = props => {
+    return (
+        <div className="col-sm-4 center-col">
+            <div className="card shadow bg-white rounded">
+                <div className="card-body">
+                    <img src={props.src} alt={props.alt} id={props.id} />
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">{props.text}</p>
+                    <Link to={props.toPage} className="btn btn-primary">{props.btnText}</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default NarrowImageCard;
