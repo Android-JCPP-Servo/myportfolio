a9c2eac8ba187618d002dd43923c048f8e9bea8f
Android-JCPP-Servo <astewart1138@gmail.com>
2023-06-02 17:21:27 -0500

    Fix method of display prop children

--- src/WideImageCard.js
+++ src/WideImageCard.js
@@ -1,26 +1,34 @@
 const WideImageCard = props => {
+    const rowsOuter = [];
+    const rowsInner = [];
+    for (let i = 1; i < props.children.length; i++) {
+        if (i >= 5) {
+            rowsInner.push(props.children[i]);
+        } else {
+            rowsOuter.push(props.children[i]);
+        }
+    }
+    const handleInnerText = (e) => {
+        if (e.target.innerText == "Read more...") {
+            e.target.innerText = "Read less...";
+        } else {
+            e.target.innerText = "Read more...";
+        }
+    }
     return (
         <div className="col-md-12 shadow p-3 bg-white rounded center-col">
             <div className="row">
                 <div className="col-md-3">
-                    <img src={props.img} alt={props.alt} id={props.id}/>
+                    {props.children[0]}
                 </div>
                 <div className="col-md-6">
-                    {props.title && <h1>{props.title}</h1>}
-                    {props.p1 && <p>{props.p1}</p>}
-                    {props.p2 && <p>{props.p2}</p>}
-                    {props.p3 && <p>{props.p3}</p>}
-                    <div className="collapse" id="readMore">
-                        {props.p4 && <p>{props.p4}</p>}
-                        {props.p5 && <p>{props.p5}</p>}
-                        {props.p6 && <p>{props.p6}</p>}
-                        {props.p7 && <p>{props.p7}</p>}
-                        {props.p8 && <p>{props.p8}</p>}
-                        {props.p9 && <p>{props.p9}</p>}
-                    </div>
-                    <button className="btn btn-primary readMore" type="button" data-bs-toggle="collapse" data-bs-target="#readMore" aria-expanded="false" aria-controls="readMore" id="readMoreButton">
-                        Read more...
-                    </button>
+                    {rowsOuter.length != 0 && rowsOuter}
+                    {
+                       rowsInner.length != 0 && <div>
+                          <div className="collapse" id="readMore">{rowsInner}</div>
+                          <button className="btn btn-primary readMore" type="button" data-bs-toggle="collapse" data-bs-target="#readMore" aria-expanded="false" aria-controls="readMore" id="readMoreButton" onClick={handleInnerText}>Read more...</button>
+                       </div>
+                    }
                 </div>
                 <div className="col-md-3"></div>
             </div>
