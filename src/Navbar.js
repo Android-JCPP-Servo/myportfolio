e4827c5afadce8e77001eedcf6eeb51286de24c9
Android-JCPP-Servo <astewart1138@gmail.com>
2023-06-02 22:04:05 -0500

    Add projects and certifications to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -22,20 +22,46 @@ const Navbar = () => {
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Projects</a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><a href="#" className="dropdown-item" style={{fontWeight: 'bold'}}>Completed</a></li>
+
+                                <li><a className="dropdown-item" href="https://cyberlandr.com" target="_blank">CyberLandr<sup>TM</sup></a></li>
+
+                                <li><a className="dropdown-item" href="https://play.google.com/store/apps/details?id=com.team02.boma" target="_blank">BOMA! BMI Calculator</a></li>
+
                                 <li><hr className="dropdown-divider"/></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+
+                                <li><a href="#" className="dropdown-item" style={{fontWeight: 'bold'}}>In-Progress</a></li>
+
+                                <li><a className="dropdown-item" href="#">JACK - AI Voice Assistant</a></li>
                             </ul>
                         </li>
 
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Certifications</a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><hr className="dropdown-divider"/></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            <li className="dropdown-item"><strong>Completed</strong></li>
+
+                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/65f0ff88f4fc58e0536b3b51648dff24" target="_blank">C#</a></li>
+
+                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/af00e5032d0a68cc84879983f5d8333b" target="_blank">React</a></li>
+
+                            <li><a className="dropdown-item" href="https://www.linkedin.com/learning/certificates/e321dc294a21173a1cdcb34c26a6287d5a50ea05282dd1f12fb26a4258f93923" target="_blank">Learning the OWASP Top 10</a></li>
+
+                            <li><a className="dropdown-item" href="https://www.linkedin.com/learning/certificates/2236364fa677cc98311b70a004669d319ff6d56f4f172862cbb9722cbadfa9c2?u=2153100" target="_blank">Secure Coding in Python</a></li>
+
+                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/a8ab218d5950c29861635cc0bf12fd13" target="_blank">Git & GitHub</a></li>
+
+                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/705dcb15de0da4dd9d9fc4f3274b430e" target="_blank">JavaScript</a></li>
+
+                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/9a5bb1fc45b4281af1fffec93b0aaf05" target="_blank">CSS</a></li>
+
+                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/9eb0741e5ebef1f9f58a53bfac67d3a7" target="_blank">HTML</a></li>
+
+                            <li><a className="dropdown-item" href="https://www.sololearn.com/Certificate/1059-23320258/jpg" target="_blank">PHP</a></li>
+
+                            <li><hr className="dropdown-divider" /></li>
+                            <li className="dropdown-item"><strong>In-Progress</strong></li>
+                            <li><a className="dropdown-item" href="https://www.codecademy.com/career-journey/full-stack-engineer" target="_blank">Full-Stack Web Development</a></li>
                             </ul>
                         </li>
                     </ul>
