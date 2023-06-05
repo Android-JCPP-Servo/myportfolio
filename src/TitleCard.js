6c38be6c61ea7c8839423989c351e3a5aa72c504
Android-JCPP-Servo <astewart1138@gmail.com>
2023-06-05 15:58:48 -0500

    Add styling for TitleCard grid layout

--- src/TitleCard.js
+++ src/TitleCard.js
@@ -2,8 +2,15 @@ const TitleCard = props => {
     return (
         <div className="col-md-3 shadow p-3 bg-white rounded center-col">
             <div className="row">
-                <div className="col-md-12 text-center">
+                <div className="col-md-12 text-start">
                     <h2>{props.pageTitle}</h2>
+                    {props.children && 
+                        <div className="row work-social">
+                            <div className="col-md-12">{props.children[0]}</div>
+                            <div className="col-md-12">{props.children[1]}</div>
+                            <div className="col-md-12">{props.children[2]}</div>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
