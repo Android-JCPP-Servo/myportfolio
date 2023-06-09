a8f2a5c651b8c3bd2d32a65f85ccbd47f2268a1b
Android-JCPP-Servo <astewart1138@gmail.com>
2023-06-08 19:28:08 -0500

    Remove certification dropdown

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,37 +9,7 @@ const Navbar = () => {
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Certifications</a>
-                            <ul className="dropdown-menu">
-                            <li className="dropdown-item"><strong>Completed</strong></li>
-
-                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/65f0ff88f4fc58e0536b3b51648dff24" target="_blank">C#</a></li>
-
-                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/af00e5032d0a68cc84879983f5d8333b" target="_blank">React</a></li>
-
-                            <li><a className="dropdown-item" href="https://www.linkedin.com/learning/certificates/e321dc294a21173a1cdcb34c26a6287d5a50ea05282dd1f12fb26a4258f93923" target="_blank">Learning the OWASP Top 10</a></li>
-
-                            <li><a className="dropdown-item" href="https://www.linkedin.com/learning/certificates/2236364fa677cc98311b70a004669d319ff6d56f4f172862cbb9722cbadfa9c2?u=2153100" target="_blank">Secure Coding in Python</a></li>
-
-                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/a8ab218d5950c29861635cc0bf12fd13" target="_blank">Git & GitHub</a></li>
-
-                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/705dcb15de0da4dd9d9fc4f3274b430e" target="_blank">JavaScript</a></li>
-
-                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/9a5bb1fc45b4281af1fffec93b0aaf05" target="_blank">CSS</a></li>
-
-                            <li><a className="dropdown-item" href="https://www.codecademy.com/profiles/Android_Servo/certificates/9eb0741e5ebef1f9f58a53bfac67d3a7" target="_blank">HTML</a></li>
-
-                            <li><a className="dropdown-item" href="https://www.sololearn.com/Certificate/1059-23320258/jpg" target="_blank">PHP</a></li>
-
-                            <li><hr className="dropdown-divider" /></li>
-                            <li className="dropdown-item"><strong>In-Progress</strong></li>
-                            <li><a className="dropdown-item" href="https://www.codecademy.com/career-journey/full-stack-engineer" target="_blank">Full-Stack Web Development</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
 
                     {/* <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
