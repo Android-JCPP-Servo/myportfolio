256ce20925d5ec582188863ae7020eafe0e770d0
Android-JCPP-Servo <astewart1138@gmail.com>
2023-06-10 14:08:20 -0500

    Switch to Link instead of useHistory

--- src/Return.js
+++ src/Return.js
@@ -1,7 +1,6 @@
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Return = () => {
-    let history = useHistory();
     return (
         <div className="col-md-3 shadow p-3 bg-white rounded center-col">
             <div className="row">
